6bcfd6e8614d951544e47ad4ac8943ae10f2efa4
Emilse Arias <emilsearias@gmail.com>
2023-12-12 10:33:46 -0300

    agregamos un state en el componente Contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,19 +1,20 @@
-const Contador = () => {
-  let numero = 10;
+import { useState } from "react";
 
+const Contador = () => {
+//   let numero = 10;
+const [numero, setNumero] = useState(10);
+//numero = numero +1
   return (
     <article className="text-center mt-4">
       <h3>Contador</h3>
       <p className="fw-bold">{numero}</p>
       <button
         className="btn btn-primary"
-        onClick={() => {
-          numero++;
-          console.log(numero);
-        }}
+        onClick={() => setNumero(numero + 1) }
       >
         +1
       </button>
+      {/* Todo: agregar un boton para decrementar el valor de numero */}
     </article>
   );
 };
